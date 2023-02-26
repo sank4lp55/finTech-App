05bcbfbd71a5f71c742b0c9788f03b583280346e
sank4lp55 <sankalpsrivastav55@gmail.com>
2023-02-26 06:36:28 +0530

    NavBar Setup

---
+++ frontend/lib/navigation_container.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:frontend/views/pages/expense_folder/expense_manager.dart';
+import 'package:frontend/views/pages/homepage.dart';
+import 'package:frontend/views/pages/investment.dart';
+import 'package:frontend/views/pages/profile.dart';
+import 'package:frontend/views/pages/expense_folder/statistics.dart';
+
+class NavigationContainer extends StatefulWidget {
+  const NavigationContainer({super.key});
+
+  @override
+  State<NavigationContainer> createState() => _NavigationContainerState();
+}
+
+class _NavigationContainerState extends State<NavigationContainer> {
+  var currentIndex = 0;
+
+  //
+
+  List Screen = [Homepage(), ExpenseManager(), Investment(), Profile()];
+  //
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Screen[currentIndex],
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.all(20),
+        height: size.width * .155,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.15),
+              blurRadius: 30,
+              offset: Offset(0, 10),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: ListView.builder(
+          itemCount: 4,
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.symmetric(horizontal: size.width * .024),
+          itemBuilder: (context, index) => InkWell(
+            onTap: () {
+              setState(
+                () {
+                  currentIndex = index;
+                },
+              );
+            },
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 1500),
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  margin: EdgeInsets.only(
+                    bottom: index == currentIndex ? 0 : size.width * .029,
+                    right: size.width * .0422,
+                    left: size.width * .0422,
+                  ),
+                  width: size.width * .128,
+                  height: index == currentIndex ? size.width * .014 : 0,
+                  decoration: BoxDecoration(
+                    color: Color(0xff2B3467),
+                    borderRadius: BorderRadius.vertical(
+                      bottom: Radius.circular(10),
+                    ),
+                  ),
+                ),
+                Icon(
+                  listOfIcons[index],
+                  size: size.width * .076,
+                  color: index == currentIndex
+                      ? Color(0xff2B3467)
+                      : Colors.black38,
+                ),
+                SizedBox(height: size.width * .03),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<IconData> listOfIcons = [
+    Icons.home_rounded,
+    Icons.favorite_rounded,
+    Icons.settings_rounded,
+    Icons.person_rounded,
+  ];
+}
