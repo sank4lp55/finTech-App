148794057f5de369ad75dad72c630590c7f98650
abhinavv9 <abhinavvrm14@gmail.com>
2023-03-06 00:51:04 +0530

    Transaction module complete || Auth complete

---
+++ backend/src/models/transaction.js
@@ -0,0 +1,49 @@
+
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const transaction = sequelize.define('transaction', {
+        type: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        amount: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        category: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        transaction_sender: {
+            type: DataTypes.SMALLINT,
+            allowNull: false,
+        },
+        transaction_receiver: {
+            type: DataTypes.SMALLINT,
+            allowNull: false,
+        },
+
+    }, {});
+    transaction.associate = function (models) {
+        // associations can be defined here
+        transaction.belongsTo(models.user, {
+            foreignKey: 'transaction_sender',
+            as: 'sender',
+            onDelete: 'CASCADE'
+        });
+        transaction.belongsTo(models.user, {
+            foreignKey: 'transaction_receiver',
+            as: 'receiver',
+            onDelete: 'CASCADE',
+        });
+    };
+    return transaction;
+};
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,11 +4,11 @@ const router = express.Router();
 // Importing Route Module
 const user = require("./modules/user.routes");
 const auth = require("./modules/auth.routes");
-
+const transaction = require("./modules/transaction.routes");
 
 // Importing Routes
 router.use("/v1/users", user);
 router.use("/v1/auth", auth);
-
+router.use("/v1/trxn", transaction);
 
 module.exports = router;
